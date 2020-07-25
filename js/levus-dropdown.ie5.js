57f556e3e8d88538dac184aed3b8d950f00a01f5
duncan <lvivduncan@gmail.com>
2020-07-25 17:38:00 +0300

    add min.js and ie5.js

--- js/levus-dropdown.ie5.js
+++ js/levus-dropdown.ie5.js
@@ -1,31 +1,22 @@
+// 25-07-2020
 "use strict";
 
-// 25-07-2020
 function levusDropdown() {
-    // усі вкладені УЛи в НОН
-    document.querySelectorAll('.levus-dropdown ul').forEach(function(element) {
-        return element.style.display = 'none';
-    }); // УЛ при наведенні стає блок
-
+    // УЛ при наведенні стає блок
     document.querySelectorAll('.levus-dropdown').forEach(function(element) {
-        return element.addEventListener('mouseover', function() {
+        return element.addEventListener('mousedown', function() {
             var _this = this;
 
             setTimeout(function() {
-                _this.querySelector('ul').style.display = 'block';
+                // add better toggle because click on html close elements
+                _this.querySelector('ul').classList.add('open');
             }, 300);
         });
     });
-    document.querySelectorAll('.levus-dropdown').forEach(function(element) {
-        return element.addEventListener('mouseout', function() {
-            var _this2 = this;
-
-            setTimeout(function() {
-                _this2.querySelector('ul').style.display = 'none';
-            }, 1200);
+    document.querySelector('html').addEventListener('click', function() {
+        document.querySelectorAll('.levus-dropdown ul').forEach(function(element) {
+            return element.classList.remove('open');
         });
     });
 } // init
-
-
 levusDropdown();
