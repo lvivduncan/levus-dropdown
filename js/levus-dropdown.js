056186052d32d464af186ee298efa652159ff2b2
duncan <lvivduncan@gmail.com>
2021-04-14 16:10:39 +0300

    rewrite to jquery

--- js/levus-dropdown.js
+++ js/levus-dropdown.js
@@ -1,12 +1,15 @@
 
+// вставка враппера
 $('body').append('<div class="levus-dropdown-wrapper"></div>');
 
+// клік на елементі
 $('.levus-dropdown a').on('click', function(e){
     e.preventDefault();
     $(this).next('.levus-dropdown-content').toggleClass('open');
     $('.levus-dropdown-wrapper').toggleClass('open');
 });
 
+// клік на враппері
 $('.levus-dropdown-wrapper').on('click', function(){
     $('.levus-dropdown-content').removeClass('open');
     $(this).removeClass('open');
